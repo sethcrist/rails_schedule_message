f9fec2c9f69f64265d6ba0ab6c587e27c67bbefc
Seth Crist <sethcrist50@gmail.com>
2024-04-07 16:50:39 -0500

    added a create login method

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  get "about-us", to: "about#index", as: :about
+  get "about", to: "about#index"
 
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
+
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+
   delete "logout", to: "sessions#destroy"
 
   root to: "main#index"
