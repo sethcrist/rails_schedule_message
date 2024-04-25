60a7ed767c2232fcdc7da9d7861fc161f4fe21c9
Seth Crist <sethcrist50@gmail.com>
2024-04-25 15:09:50 -0500

    added a password reset update

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,6 +1,5 @@
 class PasswordResetsController < ApplicationController
   def new
-
   end
 
   def create
@@ -10,5 +9,25 @@ class PasswordResetsController < ApplicationController
     end
 
       redirect_to root_path, notice: "If that account was found with that email, we have sent a link to reset your password"
+  end
+
+  def edit
+    @user = User.find_signed!(params[:token], purpose: "password_reset")
+  rescue ActiveSupport::MessageVerifier::InvalidSignature
+    redirect_to sign_in_path, alert: "Your token has expired. Please try again."
+  end
+
+  def update
+    @user = User.find_signed!(params[:token], purpose: "password_reset")
+    if @user.update(password_params)
+      redirect_to sign_in_path, notice: "Your password was reset successfully. Please sign in."
+    else
+      render :edit
+    end
+  end
+
+  private
+  def password_params
+    params.require(:user).permit(:password, :password_confirmation)
   end
 end
