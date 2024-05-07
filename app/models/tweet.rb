998b2a376c30897341d45d300099d7136ca4baf0
Seth Crist <sethcrist50@gmail.com>
2024-05-07 13:23:03 -0500

    created validations for tweets

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,11 @@
 class Tweet < ApplicationRecord
   belongs_to :user
   belongs_to :twitter_account
+
+  validates :body, length: {minimum: 1, maximum: 280 }
+  validate :publish_at, presence: true
+
+  after_initialize do
+    self.publish_at ||= 14.hour.form_now
+  end
 end
