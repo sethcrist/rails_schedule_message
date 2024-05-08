31b19daf2e1575ab2f78492d8a946e4c116c7174
Seth Crist <sethcrist50@gmail.com>
2024-05-07 19:01:03 -0500

    added a background job for posting tweets that will keep track of different scheduling

---
+++ app/jobs/tweet_job.rb
@@ -0,0 +1,12 @@
+class TweetJob < ApplicationJob
+  queue_as :default
+
+  def perform(tweet)
+    return if tweet.published?
+
+    # Rescheduled a tweet to a future date
+    return if tweet.publish_at > Time.current
+
+    tweet.publish_to_twitter!
+  end
+end
